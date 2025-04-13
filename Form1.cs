60f08c9637e148f64269eb3fc77254c5da5b8a1a
eduacct <199309935+eduacct@users.noreply.github.com>
2025-04-13 17:21:31 +0300

    fixed try-catch

--- Form1.cs
+++ Form1.cs
@@ -14,11 +14,10 @@ namespace lab08_swe_lab
 
         private void calc_Click(object sender, EventArgs e)
         {
-            double ve = double.Parse(exVelo.Text);
-            double mi = double.Parse(minit.Text);
-            double mf = double.Parse(mfinal.Text);
-            try
-            {
+            try {
+                double ve = double.Parse(exVelo.Text);
+                double mi = double.Parse(minit.Text);
+                double mf = double.Parse(mfinal.Text);
                 double dv = ve * Math.Log(mi / mf);
                 result.Text = dv.ToString();
                 result.Visible = true;
